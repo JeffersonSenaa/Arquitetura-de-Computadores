e0d486a815810fcd56ab88d4f26c2786b667287a
Jefferson12144 <jeffersonsena12144@gmail.com>
2023-02-03 16:08:17 -0300

    Criação do contador e função para armazenar o resultado nos registradores hi e lo.

--- multifac.asm
+++ multifac.asm
@@ -1,25 +1,32 @@
 .data
 
 .text
-    main:
-    li $a0, 9  #Multiplicando #Manter esse registrador por enquanto
-
-    li $a1, 3  #Multiplicador
-
-    move $s0, $zero    #Variavel i do loop
-    move $a2, $zero    #Iniciando o registrador que vai receber o resultado
+main:
     multifac:
-        slt $t0, $s0, $a1   # i < n --> $t0=1, se nao $t0=0.
+    li $a0, 170000000  #Multiplicando 
+    li $a1, 1000  #Multiplicador
+
+    move $s1, $a0
+    move $s2, $a1
+
+    move $s0, $zero         #Variavel i do loop
+    contador:
+        slt $t0, $s0, $s2   # i < n --> $t0=1, se nao $t0=0.
         beq $t0, $zero, resultado
 
-        add $a2, $a2, $a0   #Somando o multiplicando com ele mesmo 
+        jal multiplicar     #Salto para a soma salvando a posicao
                            
         addi $s0, $s0, 1    #Incremento do loop
-            j multifac
+        j contador
 
-    resultado:
-        #mfhi rd 
-        #mflo $s1
+    multiplicar:
+         addu $s3, $s3, $s1  #Somando o multiplicando com ele mesmo 
+         jr $ra             #Salto de retorno ao loop
 
-        li $v0, 10
+    resultado:     
+        srl $t0, $s3, 31 
+        mthi $t0            #Esse valor é guardado no registrador alto
+        mtlo $s3            #O restante do valor(bits a direita) sao guardadas no registrados baixo
+
+        li $v0, 10          #Comando para finalizar algoritmo
         syscall
